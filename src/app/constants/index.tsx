07c6d3fe54d1546b5b9162191efd647203829943
kristof-z <kristofzorko@gmail.com>
2025-01-14 00:22:00 +0100

    add tailwind redux modal library to projects

--- src/app/constants/index.tsx
+++ src/app/constants/index.tsx
@@ -1,40 +1,47 @@
 import { ProjectCardProps } from "../components/types";
 
 export const skills: string[] = [
-  "ReactJS",
+  "React",
   "Node.js",
   "REST API",
   "NextJS",
   "Express",
-  "MongoDB",
-  "Mono-repo",
-  "Storybook"
+  "MongoDB"
 ];
 
 const NELICAT_LINK = 'https://www.nelicat.com';
 const WRNFT_LINK = 'https://frontend-wrnft.vercel.app/';
-const TAB_SESSION_LINK_GITHUB = 'https://github.com/kristof-z/tab-session'
+const TAB_SESSION_LINK_GITHUB = 'https://github.com/kristof-z/tab-session';
+const TAILWIND_REDUX_MODAL_LINK_GITHUB = 'https://github.com/kristof-z/react-modal-tailwind';
+
 export const GITHUB_LINK = 'https://github.com/kristof-z'
 export const EMAIL = 'contact@kristofzorko.com'
 
 export const projects: ProjectCardProps[] = [
   {
-    title: 'Nelicat',
-    description: 'A lovely landing page for a cat hotel made with Next.js.',
-    link: NELICAT_LINK,
+    title: 'Tailwind Redux Modal',
+    description: 'A flexible and customizable modal component library.',
+    github: TAILWIND_REDUX_MODAL_LINK_GITHUB,
     index: 0, 
   },
   {
-    title: 'wrNFT',
-    description: 'wrNFT is a demo application that showcases the concept of NFT wrapping. This project illustrates how users can wrap their existing NFTs into a new format, adding an additional layer of functionality, flexibility, or metadata to their assets.',
-    link: WRNFT_LINK,
+    title: 'Nelicat',
+    description: 'SEO Optimized Next.js App',
+    link: NELICAT_LINK,
     index: 1, 
   },
   {
-    title: 'Tab Session',
-    description: 'Simple chrome extension that tracks browser session time and individual tab session time, helping users manage their browsing habits and productivity.',
-    github: TAB_SESSION_LINK_GITHUB,
+    title: 'wrNFT',
+    description: 'React App utilizing Framer Motion',
+    link: WRNFT_LINK,
     index: 2, 
+  },
+
+  {
+    title: 'Tab Session',
+    description: 'Chrome extension that tracks browser session time.',
+    github: TAB_SESSION_LINK_GITHUB,
+    index: 3, 
   }
 ];
 
